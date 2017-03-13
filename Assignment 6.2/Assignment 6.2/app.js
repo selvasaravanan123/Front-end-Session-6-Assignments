cccac36f10862cbdf3b3afa0ff8d676b5f86bca4
selvasaravanan123 <selvasaravanan123@gmail.com>
2017-03-13 16:37:03 +0530

    Assignment 6.2

---
+++ Assignment 6.2/Assignment 6.2/app.js
@@ -0,0 +1,24 @@
+
+
+var a=10,b=18,c=7;
+
+/* checks a>b and a>c  if  both conditions true, A is greater */
+
+if (a>b  && a>c)
+{
+
+console.log("A is Greater : " + a);
+
+}
+/* checks b>a  and b>c  if  both conditions true, b is greater */
+
+if (b>a  && b>c)
+{
+console.log("B is Greater : " + b);
+}
+/* if the above two conditions were false c is greater*/
+
+else
+{
+console.log("C is Greater : " + c);
+}
